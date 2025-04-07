96e62c37a9c4d627881024f0056fcca73c6276dc
rganjare <rahul.ganjare@gmail.com>
2025-04-07 21:38:43 +0530

    adding new test case

---
+++ src/test/java/com/companyname/projectname/modulename/rough/NewTestCase.java
@@ -0,0 +1,10 @@
+package com.companyname.projectname.modulename.rough;
+
+public class NewTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mitika created new Test case");
+	}
+
+}
